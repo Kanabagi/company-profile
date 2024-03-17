025b24055e1419902da0d0b1fa07bef740999c55
16_shrr <ctbaby1626@gmail.com>
2024-03-17 21:12:57 +0700

    Contact Done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { contactImg } from '@/constants/data'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <>
+      <section className='bg-hero bg-cover bg-center w-full h-[150px] md:h-[200px] flex items-center'>
+        <span className='wrapper font-bold text-blue-800 text-[24px] sm:text-[30px] md:text-[40px]'>
+          Contact Us
+        </span>
+      </section>
+
+      <section className='wrapper flex flex-col md:flex-row gap-10 w-full py-10 '>
+        <div className='flex flex-col gap-4 w-full'>
+          <div className='flex flex-row md:flex-col gap-2 md:gap-4'>
+            <Input type='text' placeholder='Name' className='border-0 bg-[#f0f0f0] outline-offset-0 placeholder:text-gray-400 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-full px-6 h-[54px] text-[16px]' />
+            <Input type='email' placeholder='Email' className='border-0 bg-[#f0f0f0] outline-offset-0 placeholder:text-gray-400 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-full px-6 h-[54px] text-[16px]' />
+          </div>
+          <Textarea placeholder='Message' className='border-0 bg-[#f0f0f0] outline-offset-0 placeholder:text-gray-400 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0 rounded-[24px] px-6 py-4 text-[16px] h-[150px]' />
+          <Button className='bg-blue-800 rounded-full h-[54px]'>
+            Submit
+          </Button>
+        </div>
+
+        <div className='w-full flex flex-col gap-4'>
+          <h1 className='font-bold text-[24px] sm:text-[30px] md:text-[40px]'>
+            <span className='text-blue-800'>Address</span> Info
+          </h1>
+
+          <p className='text-[16px] md:text-[20px]'>
+            Office 24 Building, block 112-A Jl. Kahuripan Kav. 16-19 Tokyo, Japan.
+          </p>
+
+          <div className='flex md:flex-col gap-6 md:gap-4 flex-wrap'>
+            {contactImg.map((contact) => (
+              <Link href={contact.path} className='flex gap-2 md:gap-4 group' target='blank'>
+                <Image src={contact.iconUrl} alt='' width={28} height={28} className='w-[20px] h-[20px] md:w-[28px] md:h-[28px] group-hover:rotate-12' />
+                <p className='text-[16px] md:text-[20px] group-hover:text-blue-800'>{contact.desc}
+                </p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- constants/data.ts
+++ constants/data.ts
@@ -39,3 +39,24 @@ export const workProcess = [
         iconUrl: '/assets/icons/coding.png'
     },
 ]
+
+export const contactImg = [
+    {
+        name: 'WhatsApp',
+        desc: '+62 8211 8437 032',
+        iconUrl: '/assets/icons/wa.svg',
+        path: 'https://wa.link/96hnsw',
+    },
+    {
+        name: 'Email',
+        desc: 'kanabagi@kjt.id',
+        iconUrl: '/assets/icons/email.svg',
+        path: "mailto:'kanabagigroup@gmail.com'",
+    },
+    {
+        name: 'Address',
+        desc: 'Kahuripan, Tokyo, Japan',
+        iconUrl: '/assets/icons/house.svg',
+        path: 'https://www.google.com/maps/place/Adachi,+Tokyo,+Jepang/@35.7780404,139.7560251,13z/data=!3m1!4b1!4m15!1m8!3m7!1s0x605d1b87f02e57e7:0x2e01618b22571b89!2sTokyo,+Jepang!3b1!8m2!3d35.6764225!4d139.650027!16zL20vMDdkZms!3m5!1s0x601891dc68347377:0xc180870e7f484240!8m2!3d35.775017!4d139.8044129!16zL20vMDF4aHB4?entry=ttu',
+    },
+]
